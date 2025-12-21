0f333931fcf33a65667bf37c91389eeeb3480fff
Ra-Zin <rajinmaharjan41@gmail.com>
2025-12-22 00:58:16 +0545

    php 2 commit

---
+++ week5/includes/footer.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <style>
+            footer{
+                background-color: #ddd;
+                padding: 20px;
+                text-align: center;
+            }
+        </style>
+
+        <footer>
+            <p> &copy; Rajin Maharjan 2025 </p>
+        </footer>  
+    </body>
+</html>
